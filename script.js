26feb3a6502c7c75418dbc8011f2396d21a470e2
Anshul Anand <iamanshulanand@gmail.com>
2023-02-07 12:39:34 +0530

    update

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ if (close){
         nav.classList.remove('active');
     })
 }
+
+
+
+
+var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
